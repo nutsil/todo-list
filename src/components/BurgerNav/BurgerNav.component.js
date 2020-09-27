47bccab46bf45fe0a2807d94fb867ffd13885dbb
nutsil <silpagui.sa@gmail.com>
2020-09-27 20:53:08 -0300

    Add BurgerNav.styles

--- src/components/BurgerNav/BurgerNav.component.js
+++ src/components/BurgerNav/BurgerNav.component.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "./BurgerNav.styles.scss";
 
 function BurgerNav() {
-  return <button></button>;
+  return (
+    <button className="burger-nav">
+      <span className="burger-nav-line"></span>
+      <span className="burger-nav-line"></span>
+      <span className="burger-nav-line"></span>
+    </button>
+  );
 }
 
 export default BurgerNav;
