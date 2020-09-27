ee6d4a19456190c3109781cdeb74347759b9ffee
nutsil <silpagui.sa@gmail.com>
2020-09-27 20:53:54 -0300

    Add BurgerNav to Header component

--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./Header.styles.scss";
+import BurgerNav from "../BurgerNav/BurgerNav.component";
 
 function Header() {
   return (
     <header className="header-title">
+      <BurgerNav />
       <h1>Website todo</h1>
     </header>
   );
