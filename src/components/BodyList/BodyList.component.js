208015f95ea1f4e12579469cf3a233e9e538f349
nutsil <silpagui.sa@gmail.com>
2020-09-27 21:36:20 -0300

    Add TaskList component in BodyList.component

--- src/components/BodyList/BodyList.component.js
+++ src/components/BodyList/BodyList.component.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./BodyList.styles.scss";
+import TaskList from "../TaskList/TaskList.component";
 
 function BodyList() {
-  return <div className="body-list"></div>;
+  return (
+    <div className="body-list">
+      <TaskList />
+    </div>
+  );
 }
 
 export default BodyList;
