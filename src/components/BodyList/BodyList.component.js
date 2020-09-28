03d844d5244f484927159c00195427082189853d
nutsil <silpagui.sa@gmail.com>
2020-09-27 22:06:27 -0300

    Add NewTaskButton to BodyList component

--- src/components/BodyList/BodyList.component.js
+++ src/components/BodyList/BodyList.component.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./BodyList.styles.scss";
 import TaskList from "../TaskList/TaskList.component";
+import NewTaskButton from "../NewTaskButton/NewTaskButton.component";
 
 function BodyList() {
   return (
     <div className="body-list">
       <TaskList />
+      <NewTaskButton />
     </div>
   );
 }
