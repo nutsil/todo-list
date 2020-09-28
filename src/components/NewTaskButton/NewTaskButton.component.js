6bb2cb83ba3a05988305f5823aadfa4ab2eeb135
nutsil <silpagui.sa@gmail.com>
2020-09-27 22:05:25 -0300

    Create NewTaskButton component

---
+++ src/components/NewTaskButton/NewTaskButton.component.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./NewTaskButton.styles.scss";
+
+function NewTaskButton() {
+  return <button className="new-task-button">+ New task</button>;
+}
+
+export default NewTaskButton;
