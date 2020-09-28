c03069898a9594348dfe15a0b7cd0873f80ad362
nutsil <silpagui.sa@gmail.com>
2020-09-27 21:35:19 -0300

    Create TaskList component

---
+++ src/components/TaskList/TaskList.component.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./TaskList.styles.scss";
+
+function TaskList() {
+  return (
+    <ul className="task-list">
+      <li>Patata</li>
+      <li>Tomato</li>
+    </ul>
+  );
+}
+
+export default TaskList;
