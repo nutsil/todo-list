da1d369a3fcf5e85bda41d43e324200f53ef2add
nutsil <silpagui.sa@gmail.com>
2020-09-27 21:16:07 -0300

    Add component BodyList to App.component.js

--- src/components/App/App.component.js
+++ src/components/App/App.component.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.styles.scss";
 import Header from "../Header/Header.component";
+import BodyList from "../BodyList/BodyList.component";
 
 function App() {
   return (
     <div className="main-wrapper">
       <Header />
+      <BodyList />
     </div>
   );
 }
